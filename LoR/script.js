1a7460aa069116fb242180f1d6dce051823e6c84
Dave Mendoza <ddcmendoza@gmail.com>
2021-02-08 16:57:45 +0800

    bg added and selectable server

--- LoR/script.js
+++ LoR/script.js
@@ -1,15 +1,19 @@
 
 
+
 const APIKEY = config.MY_API_TOKEN;
 const LEADERBOARD = document.querySelector("table#leaderboard");
-
+const SERVERS = ['americas','asia','europe','sea'];
 /*
 Servers: asia, americas, europe
 
 */
 function getLeaderboards(server){
     fetch(`https://${server}.api.riotgames.com/lor/ranked/v1/leaderboards?api_key=${APIKEY}`)
-    .then(response =>  response.json())
+    .then(response =>  {
+        clearBoard(server);
+        return response.json()
+    })
     .then(data => {
         for(let i = 0; i < data.players.length; i++){
             let name = data.players[i].name;
@@ -27,13 +31,23 @@ function getLeaderboards(server){
             tr.appendChild(td_name);
             tr.appendChild(td_lp);
 
-            LEADERBOARD.appendChild(tr);
+            LEADERBOARD.children[0].appendChild(tr);
 
         }
 
     });
 }
-getLeaderboards("asia");
+
+function clearBoard(server){
+    for(let i = 0; i < SERVERS.length; i++){
+        document.getElementById(`${SERVERS[i]}`).classList.remove('active');
+    }
+    while(LEADERBOARD.children[0].children.length > 1){
+        LEADERBOARD.children[0].children[1].remove();
+    }
+    let active = document.getElementById(`${server}`);
+    active.classList.toggle("active");
+}
 
 /* function getPlayerId(server,name,tag){
      fetch(`https://${server}.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${name}/${tag}?api_key=${APIKEY}`)
