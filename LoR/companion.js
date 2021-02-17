6201772d2ba109de44b534b0d2561058901bb385
Dave Mendoza <ddcmendoza@gmail.com>
2021-02-17 20:41:17 +0800

    Game Companion update

--- LoR/companion.js
+++ LoR/companion.js
@@ -1,50 +1,92 @@
+const CARDS = set1.concat(set2,set3);
+console.log(CARDS[0])
+const CARDCODE = CARDS.map(x => x.cardCode);
+
 const PORT = '21337';
 const CONTAINER = document.getElementById('companionContainer');
-const SETS = {
-    "01": 'set1',
-    "02": 'set2',
-    "03": 'set3',
-}
+let stateCache = null;
+let stateChange = false;
+let curve = [0,0,0,0,0,0,0,0,0];
+let curve_per = [0,0,0,0,0,0,0,0,0];
 function checkDeck(){
-    setTimeout(async function(){
-    clearContainer();
-
-    let data = await fetch(`http://127.0.0.1:${PORT}/static-decklist`);
-    let data_json = await data.json();
-    if(data_json.DeckCode !== null){
-        for(card in data_json.CardsInDeck){
-            let set = SETS[card[0]+card[1]];
-            let card_img = document.createElement('img');
-            let card_img_div = document.createElement('div');
-            let img_src = `img/cards/${set}/en_us/img/cards/${card}.png`;
-            card_img.src = img_src;
-            card_img_div.classList.add('container');
-            card_img_div.classList.add('text-light');
-            card_img_div.classList.add('bg-dark');
-            card_img_div.appendChild(card_img)
-            card_img_div.innerHTML += ` x ${data_json.CardsInDeck[card]}`
-            CONTAINER.appendChild(card_img_div)
-        }
-    }
-    else{
-        CONTAINER.innerHTML = 'NO GAME IN PROGRESS'
-    }
-}, 1000);
-
     setInterval( async function(){
-    
+        try{
+        let data2 = await fetch(`http://127.0.0.1:${PORT}/positional-rectangles`);
+        let data2_json = await data2.json();
+        let gameState = await data2_json.GameState;
 
-   let data2 = await fetch(`http://127.0.0.1:${PORT}/positional-rectangles`);
-   let data2_json = await data2.json();
-   if(data2_json.gameState)
-   console.log(data2_json)
-/*
+        if(gameState !== stateCache){
+            stateChange = true;
+        }
+        else if (gameState === stateCache){
+            stateChange = false;
+        }
+        stateCache = gameState;
 
-    let data3 = await fetch(`http://127.0.0.1:${PORT}/game-result`);
-    let data3_json = await data3.json();
- */
+        if(stateChange && gameState === 'InProgress'){
+            setTimeout(async function(){
+                clearContainer();
+                let data = await fetch(`http://127.0.0.1:${PORT}/static-decklist`);
+                if(!data.ok){
+                    const message = `An error has occured: ${response.status}`;
+                    throw new Error(message);
+                }
+                let data_json = await data.json();
+                if(data_json.DeckCode !== null){
+                    curve = [0,0,0,0,0,0,0,0,0];
+                    CONTAINER.innerHTML = "Current Deck Cards:";
+                    CONTAINER.appendChild(document.createElement('br'));
+                    let subContainer = document.createElement('div');
+                    subContainer.classList.add('p-2');
+                    for(card in data_json.CardsInDeck){
+                        if(CARDS[CARDCODE.indexOf(card)].cost < 8){
+                            curve[CARDS[CARDCODE.indexOf(card)].cost] += data_json.CardsInDeck[card];
+                        }
+                        else{
+                            curve[8] += data_json.CardsInDeck[card];
+                        }
+                        let card_img = document.createElement('img');
+                        let card_img_div = document.createElement('span');
+                        let img_src = CARDS[CARDCODE.indexOf(card)].assets[0].gameAbsolutePath;
+                        card_img.src = img_src;
+                        card_img.alt = CARDS[CARDCODE.indexOf(card)].name;
+                        card_img.classList.add('onHover');
+                        card_img.classList.add('m-3');
 
-    },2500);
+                        //card_img_div.classList.add('container-sm');
+                        card_img_div.classList.add('text-light');
+                        card_img_div.classList.add('m-3');
+                        card_img_div.classList.add('bg-dark');
+                        card_img_div.appendChild(card_img)
+                        card_img_div.innerHTML += ` x ${data_json.CardsInDeck[card]}`
+                        subContainer.appendChild(card_img_div)
+                    }
+                    CONTAINER.appendChild(subContainer)
+                    CONTAINER.appendChild(document.createElement('br'));
+                    CONTAINER.innerHTML += "Curve:";
+                    curve_per =  curve.map(x => 100*x/40);
+                    let subContainer_2 = document.createElement('div');
+                    subContainer_2.innerHTML = curve_per;
+                    CONTAINER.appendChild(subContainer_2)
+                }
+            }, 1000);
+        }
+        else if(gameState !== 'InProgress'){
+                clearContainer();
+                CONTAINER.innerHTML = 'NO GAME IN PROGRESS';
+            }
+        
+
+            /* let data3 = await fetch(`http://127.0.0.1:${PORT}/game-result`);
+            let data3_json = await data3.json();
+            */
+        }
+        catch{
+            clearContainer();
+            CONTAINER.innerHTML = "Please open game!"
+        }
+
+    },1000);
 }
 
 function clearContainer(){
@@ -57,3 +99,4 @@ function clearContainer(){
 checkDeck();
 
 // http://127.0.0.1:{port}/game-result
+
