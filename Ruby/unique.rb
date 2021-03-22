eb46b10f4d3de401ee9a66e6f780b2eebcfe2a8d
Dave Mendoza <ddcmendoza@gmail.com>
2021-03-22 19:26:34 +0800

    unique

---
+++ Ruby/unique.rb
@@ -0,0 +1,14 @@
+a = [0,0,1,0,0]
+b = [1,0,0,0,0]
+c = [1,1,1,1,0]
+
+def find_uniq(arr)
+    arr.each do |x|
+        if arr.count(x) == 1
+            return x
+        end
+    end
+end
+p find_uniq(a)
+p find_uniq(b)
+p find_uniq(c)
